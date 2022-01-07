6647ded4b81aaf1fd5abca4a5d944c4434aa8718
jongkeun-kim <kjkandrea@gmail.com>
2022-01-07 12:31:19 +0900

    Add pub sub

--- counter/view.js
+++ counter/view.js
@@ -3,6 +3,7 @@ export default {
     this.countElement = document.getElementById('count')
     this.incrementButtonElement = document.getElementById('increment')
     this.decrementButtonElement = document.getElementById('decrement')
+    this._events = {}
 
     this.render(count)
     this.attachEvents();
@@ -11,7 +12,15 @@ export default {
     this.countElement.value = count;
   },
   attachEvents() {
-    this.incrementButtonElement.addEventListener('click', () => console.log('increment'))
-    this.decrementButtonElement.addEventListener('click', () => console.log('decrement'))
+    this.incrementButtonElement.addEventListener('click', () => this.emit('@increment'))
+    this.decrementButtonElement.addEventListener('click', () => this.emit('@decrement'))
+  },
+  on (eventName, callback) {
+    this._events[eventName] = callback
+
+    return this
+  },
+  emit (eventName) {
+    this._events[eventName]?.()
   }
 }
