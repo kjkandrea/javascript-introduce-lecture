3055f995402a7956bb9036f8fc2b3d4759600dff
jongkeun-kim <kjkandrea@gmail.com>
2022-01-07 12:23:11 +0900

    Add bind click evt

--- counter/view.js
+++ counter/view.js
@@ -1,10 +1,17 @@
 export default {
   init(count) {
     this.countElement = document.getElementById('count')
+    this.incrementButtonElement = document.getElementById('increment')
+    this.decrementButtonElement = document.getElementById('decrement')
 
     this.render(count)
+    this.attachEvent();
   },
   render(count) {
     this.countElement.value = count;
+  },
+  attachEvent() {
+    this.incrementButtonElement.addEventListener('click', () => console.log('increment'))
+    this.decrementButtonElement.addEventListener('click', () => console.log('decrement'))
   }
 }
