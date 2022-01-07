e633d702440498fe57f3d462b0645c08f8c2183e
jongkeun-kim <kjkandrea@gmail.com>
2022-01-07 15:17:38 +0900

    Add module source code

---
+++ 03_practice/03_module/export.js
@@ -0,0 +1,7 @@
+export function sum(a, b) {
+  return a + b
+}
+
+export function pow(a, b) {
+  return a * b
+}
---
+++ 03_practice/03_module/index.js
@@ -0,0 +1,6 @@
+import { sum, pow } from './export.js'
+
+console.log(
+sum(2, 3)
+,pow(3, 4)
+)
