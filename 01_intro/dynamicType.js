05cdcfcf772c4d3ab33ff629c63c8c484c6b5b1d
jongkeun-kim <kjkandrea@gmail.com>
2022-01-07 14:02:26 +0900

    Add dynamicType

--- 01_intro/dynamicType.js
+++ 01_intro/dynamicType.js
@@ -8,3 +8,12 @@ foo = "bar"; // foo 는 이제 String 임
 foo = true;  // foo 는 이제 Boolean 임
 
 console.log(typeof foo === 'boolean')
+
+/**
+ * 이는 하나의 변수에 할당 된 값의 유형이 언제든 변할 수 있다는 의미이며 런타임에서 다음과 같은 오류에 주의하며 작업하여야 한다.
+ */
+
+let bar = 42 // bar 는 이제 Number 임
+bar.toString() // '42'
+bar = null
+bar.toString() // Uncaught TypeError: Cannot read properties of null (reading 'toString')
